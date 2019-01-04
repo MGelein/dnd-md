a0ab3ece6946563c44324af35d6e49c877dd8d3f
Mees Gelein <meesgelein@gmail.com>
2019-01-04 17:27:21 +0100

    Added updating of links to add .md extension if no extension was found

--- js/changer.js
+++ js/changer.js
@@ -14,10 +14,34 @@ setTimeout(() => {
         //Set the classes on the body
         $('body').addClass('container');
         //And finally do the final parse
-        //parseFinal();
+        parseFinal();
     });
     //End of script, below it defines the amount of delay before continuing the script
 }, 50);
+
+/**
+ * Does multiple final checks before we're done with processing,
+ * amongst other things cleans up links, and adds .md extensions 
+ * if no extension was found
+ */
+function parseFinal() {
+    //Check all links for an extension, if none is present, add .md
+    $('a').each(function (i, link) {
+        //Get the href attribute of this link
+        let href = $(link).attr('href');
+        //See if it contains an extension (<10 letters long)
+        if (href.indexOf('.') > -1 && href.indexOf('.') > 1) {
+            if (href.substring(href.indexOf('.').length < 10)){//If the ext is shorter than 10 chars
+                console.log("skip: " + href);
+                return;//Ignore this link, it's probably fine
+            }
+        }
+        //If we reach here, we modify the link
+        href += ".md";
+        //Update the link with the new href property
+        $(link).attr('href', href);
+    });
+}
 
 /**
  * Checks if the file type we're currently viewing needs to be forwarded
@@ -35,7 +59,7 @@ function checkFileType() {
         let folder = parts[parts.length - 1].toLowerCase().trim();
         //Finally, check if any of the links on this page have the same name
         $('a.icon.file').each(function (index, link) {
-            if ($(link).text().toLowerCase() == folder + '.md' || $(link).text().toLowerCase() == 'index.md'){
+            if ($(link).text().toLowerCase() == folder + '.md' || $(link).text().toLowerCase() == 'index.md') {
                 let newDest = $(link).attr('href');
                 window.location.replace(newDest);
             }
